c6f40ffb72c13574a112b549b9d1c5c1f4623bb7
Caroline ONeill <59894440+c-oneill@users.noreply.github.com>
2025-06-17 15:12:55 -0700

    testing twilio webhook with render deploy

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
-
 import os
 from dotenv import load_dotenv
 import yagmail
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 # load environment variable
 load_dotenv()
@@ -18,19 +18,45 @@ EMAIL_RECIPIENT = os.getenv("EMAIL_RECIPIENT")
 yag = yagmail.SMTP(EMAIL_USER, EMAIL_PASS)
 
 
-
 @server.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@server.route("/test")
-def send_test_email():
+def send_email(to, subject, content):
     try:
         yag.send(
-            to=EMAIL_RECIPIENT,
-            subject="Test",
-            contents="Hello World"
+            to=to,
+            subject=subject,
+            contents=content
         )
         return "<p>Email sent successfully!</p>"
     except Exception as e:
         return f"<p>Failed to send email: {e}</p>"
+
+
+@server.route("/test")
+def send_test_email():
+    return send_email(EMAIL_RECIPIENT, "Test", "Hello World")
+
+
+@server.post("/msg")
+def recieve_test_email():
+    data = request.json
+    return send_email(data["to"], data["subject"], data["content"])
+
+
+@app.post("/sms")
+def sms_reply():
+    incoming_msg = request.form['Body']
+    from_number = request.form['From']
+    
+    print(f"Message from {from_number}: {incoming_msg}")
+    print(send_email(EMAIL_RECIPIENT, f"Twilio/Render Test, from {from_number}", incoming_msg))
+
+    # Create TwiML response
+    resp = MessagingResponse()
+    resp.message(f"You said: {incoming_msg}")
+    return str(resp)
+
+
+
